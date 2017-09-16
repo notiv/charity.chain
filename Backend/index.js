b51d51b9da173ffb18266c661fb1563a74c0993d
Manuel-Gajo <manuelgajo.mnl@gmail.com>
2017-09-16 14:47:14 +0200

    server & socket+client setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,25 +1,42 @@
-const express = require('express');
+const app = require('express')();
+const http = require('http').Server(app);
 const bodyparser = require('body-parser');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const apiRouter = require('./api/index');
 const database = require('./utils/database');
-const ethereum = require('./utils/ethereum');
+// const ethereum = require('./utils/ethereum');
 const async = require('async');
-
-const app = express();
+const io = require('socket.io')(http);
 
 app.use(bodyparser());
 app.use('/api', apiRouter);
 
+// socket.io set up
+io.on('connection', socket => {
+    // nothing yet
+
+    // test
+    const data = {
+      someData: ['hello', 'socket'],
+    };
+    io.emit('test event', data)
+});
+
+// API endpoints
+app.get('/api/highlights', (req, res) => {
+    // TODO
+    res.json({});
+});
+
 async.series([
   callback => database.initalize(callback),
-  callback => ethereum.initalize(callback),
+  // callback => ethereum.initalize(callback),
 ], (err) => {
   if (err) {
     logger.error('Unable to start server because initialisation errors', err);
   } else {
-    app.listen(config.port, (err) => {
+    http.listen(config.port, (err) => {
       if (err) {
         logger.error(`Unable to start server on port ${config.port}`, err);
       } else {
