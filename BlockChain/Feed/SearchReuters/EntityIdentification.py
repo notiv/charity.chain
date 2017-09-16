700c5aaf03bae5b3617c5f18d239f1b790aaa72f
Viton Vitanis <viton.vitanis@gmail.com>
2017-09-16 17:21:08 +0200

    Added entity identification
    Right now entity identification is not integrated into querying

--- BlockChain/Feed/SearchReuters/EntityIdentification.py
+++ BlockChain/Feed/SearchReuters/EntityIdentification.py
@@ -1,19 +1,30 @@
 # -*- coding: utf-8 -*-
 
 from SearchReuters.ReutersDataSource import ReutersDataSource
-import requests
+import requests, json
 
 CALAIS_URL = 'https://api.thomsonreuters.com/permid/calais'
+TOKEN = 'qj4g21ktlFiHqUsnXTt3XbKqBmeTOa9E'
 
 class EntityIdentification:
 
-    def __init__(self, username = USERNAME, password = PASSWORD):
-        # Initialize ReutersDataSource
-        self.rds = ReutersDataSource()
-        self.headers = {'X-AG_Access-Token' : self.rds.get('authToken'),
+    def __init__(self):
+        self.headers = {'X-AG-Access-Token' : TOKEN,
                         'Content-Type' : 'text/raw',
                         'outputformat' : 'application/json'}
 
     def identify_entity(self, input_text):
-        response = requests.post(CALAIS_URL, data = input_text, headers = self.headers, timeout = 80)
+        response = requests.post(CALAIS_URL, data=input_text, headers=self.headers, timeout=80)
+
+        if response.status_code == 200:
+            response_json_obj = json.loads(response.content.decode("utf-8"))
+
+            for key, value in response_json_obj.items():
+                # if (value.get('_type') is None) or (value.get('_typeGroup') is None):
+                #     print('None')
+                # else:
+                if (value.get('confidence') is not None) and \
+                        (value.get('confidence').get('statisticalfeature') is not None) and \
+                        (float(value.get('confidence').get('statisticalfeature'))> 0.8):
+                    print(value.get('_typeGroup', 'Empty Group') + ' -> ' + value.get('_type', 'Empty Type') + ' -> ' + value.get('name', 'Empty Name'))
 
--- BlockChain/Feed/test_entity_identification.py
+++ BlockChain/Feed/test_entity_identification.py
@@ -1,26 +1,29 @@
 # -*- coding: utf-8 -*-
 
 from SearchReuters.EntityIdentification import EntityIdentification
+import json
 
-def test_entity_identification(self):
+def test_entity_identification():
     ei = EntityIdentification()
-    ei.identify_entity(input_text = '''
+    ei.identify_entity('''
         Venezuela has offered $5 million to victims of HurricaneHarvey in the United States 
         despite a major economic crisis in the SouthAmerican country that has left millions 
         short of food and medicine.Venezuela's U.S.-based oil subsidiary Citgo, a unit of state 
-        oil companyPDVSA, will cooperate with local authorities in Houston to distribute thefunds, 
+        oil company PDVSA, will cooperate with local authorities in Houston to distribute the funds, 
         Foreign Minister Jorge Arreaza said on state television.Venezuela's socialist government 
-        has in the past given subsidized heating oilto poor Americans and sent aid to victims of 
+        has in the past given subsidized heating oil to poor Americans and sent aid to victims of 
         Hurricane Katrina in 2005.Venezuela's already strained relations with the United States 
-        took a nosedivethis year with Washington imposing various sanctions against President Nicolas
+        took a nose dive this year with Washington imposing various sanctions against President Nicolas
         Maduro's cash-strapped government.President Donald Trump went as far as to say a military 
-        intervention may be onthe cards, though U.S. officials quickly rolled that idea back.
-        Venezuela is suffering a fourth year of brutal recession, and has been rockedby political 
-        turmoil and mass street protests against Maduro.Harvey, now downgraded to a tropical storm, 
+        intervention may be on the cards, though U.S. officials quickly rolled that idea back.
+        Venezuela is suffering a fourth year of brutal recession, and has been rocked by political 
+        turmoil and mass street protests against Maduro. Harvey, now downgraded to a tropical storm, 
         bore down on eastern Texas andLouisiana on Wednesday, bringing the kind of catastrophic 
-        downpours thatparalyzed the oil hub of Houston with record rainfall and drove tens ofthousands 
+        downpours that paralyzed the oil hub of Houston with record rainfall and drove tens of thousands 
         of people from their homes.(Reporting by Girish Gupta; Editing by Andrew Cawthorne and Alistair Bell)
                         ''')
 
+    # print(resp.status_code)
+
 if __name__ == '__main__':
-    test_entity_identification()_
+    test_entity_identification()
