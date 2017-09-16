1439567fdb03c8eed30bf5a246b92bf82faa7b36
Klemen Koželj <klemen.kozelj@gmail.com>
2017-09-16 08:24:05 +0200

    Established single node BC
    Bypassed docker networking host error :(

--- BlockChain/GenesisNode/start.sh
+++ BlockChain/GenesisNode/start.sh
@@ -6,6 +6,7 @@ bootnodeURL='enode://'$bootnodeKey'@127.0.0.1:3300'
 
 rm -rf $directory'/*':
 
+<<COMMENT
 docker rm -f GenesisNode; \
 docker run \
   --detach \
@@ -15,9 +16,29 @@ docker run \
   --volume $directory:/home/ubuntu/ethereum/ipc \
 hackzurich/charity:1.0 \
   geth \
+    --identity=GenesisNode \
+    --datadir=/home/ubuntu/ethereum/ \
+    --ipcpath=/home/ubuntu/ethereum/ipc/geth.ipc \
+    --bootnodes=$bootnodeURL \
+    --maxpeers 2 \
+    --verbosity 9 \
+    --rpc \
+    --rpcaddr '127.0.0.1' \
+    --rpcport 8545 \
+    --rpcapi 'eth,net,web3,personal' \
+    --rpccorsdomain '*' \
+    --gasprice 1 \
+    --txpool.pricelimit 1 \
+    --txpool.pricebump 1 \
+    --txpool.accountslots 1024;
+COMMENT
+
+# This is bypass
+cp -R $PWD/GenesisData/* $PWD/GenesisNode/volume/
+geth \
   --identity=GenesisNode \
-  --datadir=/home/ubuntu/ethereum/ \
-  --ipcpath=/home/ubuntu/ethereum/ipc/geth.ipc \
+  --datadir=$directory \
+  --ipcpath=$directory/ipc/geth.ipc \
   --bootnodes=$bootnodeURL \
   --maxpeers 2 \
   --rpc \
@@ -25,12 +46,10 @@ hackzurich/charity:1.0 \
   --rpcport 8545 \
   --rpcapi 'eth,net,web3,personal' \
   --rpccorsdomain '*' \
-  --ws \
-  --wsaddr '127.0.0.1' \
-  --wsport 8546 \
-  --wsapi 'eth,net,web3,personal' \
-  --wsorigins '*' \
   --gasprice 1 \
+  --mine \
+  --minerthreads 1 \
+  --etherbase '0x7ed536956731bf9267d5dcb5edda732233490ed5' \
   --txpool.pricelimit 1 \
   --txpool.pricebump 1 \
   --txpool.accountslots 1024;
